218dc44d703861be42cc5e865d1acb05d5c6c686
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-16 15:00:22 +1200

    Edit a method to count the current round. The current round will increase when the play command is typed, not when the new game is typed.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,19 +6,19 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   // Count how many times the newGame is involked
-  private int countNewGame = 0;
+  private int countRound = 0;
   private String userName = null;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
-    countNewGame++; // when the new game is clicked, the countnewgame will increase
     userName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(options[0]); // Task 1 Testing 1: Welcome_message
   }
 
   public void play() {
     // Task 1 Testing 2: Play command
-    String countNewGameString = String.valueOf(countNewGame); // Change integer to string
+    countRound++; // when the new game is clicked, the countnewgame will increase
+    String countNewGameString = String.valueOf(countRound); // Change integer to string
     MessageCli.START_ROUND.printMessage(countNewGameString); // print the message
     // Task 1 Testing 3: ask for input
     boolean rightInput = true;
