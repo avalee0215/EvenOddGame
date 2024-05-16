8bb29c0b1004c5d225b516f4209e5b0fe41c99b6
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-16 13:32:52 +1200

    For testing 2, when the play command is typed, show the current number of round, I made new integer value that will count how many times the new_game is typed

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,9 +5,12 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  // Count how many times the newGame is involked
+  private static int countNewGame = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
+    countNewGame++; // when the new game is clicked, the countnewgame will increase
     MessageCli.WELCOME_PLAYER.printMessage(options[0]); // Task 1 Testing 1: Welcome_message
   }
 
