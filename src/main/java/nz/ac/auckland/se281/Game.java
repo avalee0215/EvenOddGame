1fd75e0d3ad9be206d32b3f9d786c03ece8aa52c
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-16 18:48:43 +1200

    To print out the sum of the value after each round, I create the sum that add the user typed value and the ai random value. Then, saved what the user selected(ODD or EVEN) for the next step

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -9,10 +9,12 @@ public class Game {
   private int countRound = 0;
   private String userName = null;
   private GameLevel level;
+  private Choice userChoice;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
     userName = options[0];
+    userChoice = choice;
     switch (difficulty) {
       case EASY:
         level = new Easy();
@@ -35,11 +37,12 @@ public class Game {
     // Task 1 Testing 3: ask for input
     boolean rightInput = true;
     String inputValue = null;
+    int inputInt = 0;
 
     while (rightInput) {
       MessageCli.ASK_INPUT.printMessage(); // print out the message to ask input
       String input = Utils.scanner.nextLine(); // scan input
-      int inputInt = Integer.parseInt(input); // convert to int to check it is between 0 to 5
+      inputInt = Integer.parseInt(input); // convert to int to check it is between 0 to 5
       if (inputInt < 0 || inputInt > 5) {
         MessageCli.INVALID_INPUT.printMessage();
       } else {
@@ -47,8 +50,26 @@ public class Game {
         rightInput = false;
       }
     }
+
+    String aiNumberString = level.play();
+    int aiNumberInt = Integer.parseInt(aiNumberString);
     MessageCli.PRINT_INFO_HAND.printMessage(userName, inputValue);
-    MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", level.play());
+    MessageCli.PRINT_INFO_HAND.printMessage("HAL-9000", aiNumberString);
+
+    int sum = aiNumberInt + inputInt;
+    if (sum % 2 == 0) {
+      if (userChoice == Choice.EVEN) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(String.valueOf(sum), "EVEN", userName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(String.valueOf(sum), "EVEN", "HAL-9000");
+      }
+    } else {
+      if (userChoice == Choice.ODD) {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(String.valueOf(sum), "ODD", userName);
+      } else {
+        MessageCli.PRINT_OUTCOME_ROUND.printMessage(String.valueOf(sum), "ODD", "HAL-9000");
+      }
+    }
   }
 
   public void endGame() {}
