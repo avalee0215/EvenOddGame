39268421b8ccb3f9095ef113052b8a28031ac560
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-16 13:43:39 +1200

    As we are not allowed to use the static field and we don't need it, erase the static from the private static int

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,7 +6,7 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   // Count how many times the newGame is involked
-  private static int countNewGame = 0;
+  private int countNewGame = 0;
 
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
     // the first element of options[0]; is the name of the player
@@ -18,6 +18,8 @@ public class Game {
     // Task 1 Testing 2: Play command
     String countNewGameString = String.valueOf(countNewGame); // Change integer to string
     MessageCli.START_ROUND.printMessage(countNewGameString); // print the message
+    // Task 1 Testing 3: ask for input
+    MessageCli.ASK_INPUT.printMessage();
   }
 
   public void endGame() {}
