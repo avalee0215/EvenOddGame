9501af5720783fde1e8ea3a83ffd630797168c54
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-17 04:23:52 +1200

    print out the stats messages when the showStats command is typed

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -160,5 +160,11 @@ public class Game {
       MessageCli.GAME_NOT_STARTED.printMessage();
       return;
     }
+
+    // print out the stats messages
+    MessageCli.PRINT_PLAYER_WINS.printMessage(
+        userName, String.valueOf(userWin), String.valueOf(aiWin));
+    MessageCli.PRINT_PLAYER_WINS.printMessage(
+        "HAL-9000", String.valueOf(aiWin), String.valueOf(userWin));
   }
 }
