b0023c88664e4e457561ba7120111e4146c5d5fa
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-05-17 02:15:22 +1200

    Make a string mode which save the previous mode the system used (Random or Top). When the system loses and the mode was random, the system will switch to the top strategy mode and use this to make a random number

--- src/main/java/nz/ac/auckland/se281/Hard.java
+++ src/main/java/nz/ac/auckland/se281/Hard.java
@@ -8,14 +8,27 @@ public class Hard implements GameLevel {
   private int countEven;
   private int randomNumber;
   private boolean loserOrWin;
+  private String mode;
 
   @Override
   public String play() {
     if (currentRound < 4) {
       strategy = new Random();
       this.randomNumber = strategy.randomNumber(); // First 3 rounds should use the random mode
+      this.mode = "random";
       return String.valueOf(randomNumber);
     } else {
+      // if the system lose
+      if (loserOrWin) {
+        // if the previous method was random, switch to the Top Strategy mode.
+        if (mode == "random") {
+          strategy = new TopStrategy();
+          strategy.oddorEven(countOdd, countEven, userChoice); // Update the current values
+          this.mode = "Top";
+          this.randomNumber = strategy.randomNumber();
+          return String.valueOf(randomNumber);
+        }
+      }
       return null;
     }
   }
